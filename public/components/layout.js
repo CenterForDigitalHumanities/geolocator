37decd6a4b908bc1fbe3716d37b95741b530f8f8
cubap <cubap@slu.edu>
2023-05-05 09:57:22 -0500

    Create layout.js

---
+++ public/components/layout.js
@@ -0,0 +1,51 @@
+/**
+ * Template components like headers and footers for reuse across the site.
+ * @author cubap@slu.edu
+ */
+
+class GeoPage extends HTMLElement {
+    constructor(){
+        const header = document.createElement('geo-header')
+        const footer = document.createElement('geo-footer')
+        this.prepend(header)
+        this.append(footer)
+    }
+}
+
+customElements.define("geo-page",GeoPage)
+
+class GeoHeader extends HTMLElement {
+    constructor(){
+        const header = document.createElement('geo-header')
+        const footer = document.createElement('geo-footer')
+        this.prepend(header)
+        this.append(footer)
+    }
+}
+
+customElements.define("geo-header",GeoHeader)
+
+class GeoFooter extends HTMLElement {
+    #footerTmpl = `
+    <footer class="nav nav-center text-primary is-fixed is-full-width, is-vertical-align">
+        <a target="_blank"
+            href="https://www.slu.edu/research/faculty-resources/research-computing.php"><small>&copy;2022 Research
+                Computing Group</small></a>
+        <a target="_blank" href="https://slu.edu"><img class="brand" alt="SLU logo"
+                src="https://www.slu.edu/marcom/tools-downloads/imgs/logo/left-aligned/slu_logoleftaligned_rgb.png"></a>
+        <a target="_blank" href="https://www.slu.edu/arts-and-sciences/ong-center/"><img class="brand"
+                alt="SLU RCG logo"
+                src="https://centerfordigitalhumanities.github.io/media-assets/logos/rcg-logo.png"></a>
+        <a target="_blank" href="http://rerum.io"><img class="brand" alt="Rerum logo"
+                src="https://centerfordigitalhumanities.github.io/blog/assets/images/rerum.jpg"><small>RERUM
+                v1</small></a>
+    </footer>`
+    constructor(){
+        const header = document.createElement('geo-header')
+        const footer = document.createElement('geo-footer')
+        this.prepend(header)
+        this.append(footer)
+    }
+}
+
+customElements.define("geo-footer",GeoFooter)

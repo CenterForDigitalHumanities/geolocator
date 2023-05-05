90d77ec05a891656c0c1c11921fc4ca2028859bc
cubap <cubap@slu.edu>
2023-05-05 10:39:56 -0500

    is

--- public/components/layout.js
+++ public/components/layout.js
@@ -3,7 +3,7 @@
  * @author cubap@slu.edu
  */
 
-class GeoPage extends HTMLElement {
+class GeoPage extends HTMLBodyElement {
     constructor(){
         const header = document.createElement('geo-header')
         const footer = document.createElement('geo-footer')
@@ -12,14 +12,24 @@ class GeoPage extends HTMLElement {
     }
 }
 
-customElements.define("geo-page",GeoPage)
+customElements.define("geo-page",GeoPage, {extends:"body"})
 
 class GeoHeader extends HTMLElement {
+    #headerTmpl = `
+    <header>
+    <div class="row">
+        <div>
+            <img src="https://centerfordigitalhumanities.github.io/rerum/logo.png" alt="logo">
+        </div>
+        <small class="rerum_shimmer">reconditorium eximium rerum universalium mutabiliumque</small>
+        <h1>
+            rerum geolocator
+        </h1>
+    </div>
+    </header>
+`
     constructor(){
-        const header = document.createElement('geo-header')
-        const footer = document.createElement('geo-footer')
-        this.prepend(header)
-        this.append(footer)
+
     }
 }
 
@@ -27,7 +37,7 @@ customElements.define("geo-header",GeoHeader)
 
 class GeoFooter extends HTMLElement {
     #footerTmpl = `
-    <footer class="nav nav-center text-primary is-fixed is-full-width, is-vertical-align">
+    <footer class="nav nav-center text-primary is-fixed is-full-width is-vertical-align">
         <a target="_blank"
             href="https://www.slu.edu/research/faculty-resources/research-computing.php"><small>&copy;2022 Research
                 Computing Group</small></a>
@@ -41,10 +51,9 @@ class GeoFooter extends HTMLElement {
                 v1</small></a>
     </footer>`
     constructor(){
-        const header = document.createElement('geo-header')
-        const footer = document.createElement('geo-footer')
-        this.prepend(header)
-        this.append(footer)
+        super()
+        this.attachShadow({mode:'open'})
+        this.shadowRoot.innerHTML = this.#footerTmpl
     }
 }
 

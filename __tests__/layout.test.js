457f1e973255c318c0cfa1951e2a2d0e71e075c7
Tony Obradovic <tobradovic@slu.edu>
2023-11-15 20:19:30 -0600

    Tests work when lines including 'querySelector' are removed. Final step is to figure out how to get the tests to work with the querySelector lines enabled.

--- __tests__/layout.test.js
+++ __tests__/layout.test.js
@@ -2,11 +2,7 @@
  * @jest-environment jsdom
  */
 
-const attributeChangedCallback = require('../public/components/layout')
-
-function attributeChangedCallbackMock(name, oldValue, newValue) {
-    attributeChangedCallback(name, oldValue, newValue)
-}
+const GeolocatorPreview = require('../public/components/layout')
 
 const localStorageMock = {
     getItem: jest.fn(),
@@ -17,24 +13,26 @@ const localStorageMock = {
 
 //Assign the mock to the global object
 global.localStorage = localStorageMock;
-global.attributeChangedCallback = attributeChangedCallbackMock;
 
 describe('NavPlace attributeChangedCallback', () => {
+    let instance;
 
-    beforeEach(() => { 
+    beforeAll(() => {
+        instance = new GeolocatorPreview()
         const geo = {"geoKey": "someStringSoNotNull" }
-        localStorage.setItem('geoJSON', JSON.stringify(geo));
+        localStorage.setItem('geoJSON', JSON.stringify(geo))
+        const newResource = {"@id": "somStringSoNotNull"}
+        localStorage.setItem('newResource', JSON.stringify(newResource))
     })
-    afterEach(() => { localStorage.clear() })
 
     test("context with navPlace and presentation/3 - in order", () => {
         const userObj = {"@context": [
             "http://iiif.io/api/extension/navplace/context.json",
             "http://iiif.io/api/presentation/3/context.json"
         ]}
-        localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-        attributeChangedCallbackMock(null, null, 'navPlace');
-        const actualOutput = JSON.parse(localStorageMock.getItem('newResource'))['@context'];
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'navPlace');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
         expect(actualOutput).toEqual(userObj["@context"]);
     })
 
@@ -43,13 +41,13 @@ describe('NavPlace attributeChangedCallback', () => {
             "http://iiif.io/api/presentation/3/context.json",
             "http://iiif.io/api/extension/navplace/context.json"
         ]}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'navPlace');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
         const expectedOutput = [
             "http://iiif.io/api/extension/navplace/context.json",
             "http://iiif.io/api/presentation/3/context.json"
         ]
-        localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-        attributeChangedCallbackMock(null, null, 'navPlace');
-        const actualOutput = localStorageMock.getItem('newResource');
         expect(actualOutput).toEqual(expectedOutput);
     })
 
@@ -57,95 +55,107 @@ describe('NavPlace attributeChangedCallback', () => {
         const userObj = {"@context": [
             "http://iiif.io/api/presentation/2/context.json"
         ]}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'navPlace');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
         const expectedOutput = [
             "http://iiif.io/api/extension/navplace/context.json",
             "http://iiif.io/api/presentation/3/context.json"
         ]
-        localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-        attributeChangedCallbackMock(null, null, 'navPlace');
-        const actualOutput = localStorageMock.getItem('newResource');
         expect(actualOutput).toEqual(expectedOutput);
     })
 
-    // test("non-Array context", () => {
-    //     const userObj = {"@context": "http://iiif.io/api/presentation/3/context.json"}
-    //     const expectedOutput = [
-    //         "http://iiif.io/api/extension/navplace/context.json",
-    //         "http://iiif.io/api/presentation/3/context.json"
-    //     ]
-    //     localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-    //     attributeChangedCallback(null, null, 'navPlace');
-    //     const actualOutput = localStorageMock.getItem('newResource');
-    //     expect(actualOutput).toEqual(expectedOutput);
-    // })
-    // test("random context", ()=> {
-    //     const userObj = {"@context": "http://someAddress.json"}
-    //     const expectedOutput = [
-    //         "http://someAddress.json",
-    //         "http://iiif.io/api/extension/navplace/context.json",
-    //         "http://iiif.io/api/presentation/3/context.json"
-    //     ]
-    //     localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-    //     attributeChangedCallback(null, null, 'navPlace');
-    //     const actualOutput = localStorageMock.getItem('newResource');
-    //     expect(actualOutput).toEqual(expectedOutput);
-    // })
+    test("non-Array context", () => {
+        const userObj = {"@context": "http://iiif.io/api/presentation/3/context.json"}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'navPlace');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://iiif.io/api/extension/navplace/context.json",
+            "http://iiif.io/api/presentation/3/context.json"
+        ]
+        expect(actualOutput).toEqual(expectedOutput);
+    })
+
+    test("random context", ()=> {
+        const userObj = {"@context": "http://someAddress.json"}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'navPlace');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://someAddress.json",
+            "http://iiif.io/api/extension/navplace/context.json",
+            "http://iiif.io/api/presentation/3/context.json"
+        ]
+        expect(actualOutput).toEqual(expectedOutput);
+    })
 })
 
-// describe('Annotation attributeChangedCallback', () => {
+describe('Annotation attributeChangedCallback', () => {
+    let instance;
 
-//     afterEach(() => { localStorage.clear() })
+    beforeAll(() => {
+        instance = new GeolocatorPreview()
+        const geo = {"geoKey": "someStringSoNotNull" }
+        localStorage.setItem('geoJSON', JSON.stringify(geo))
+        const newResource = {"@id": "somStringSoNotNull"}
+        localStorage.setItem('newResource', JSON.stringify(newResource))
+    })
 
-//     test("non-Array context", () => {
-//         const userObj = {"@context": "http://lived-religion.rerum.io/deer-lr/vocab/context.json"}
-//         const expectedOutput = [
-//             "http://lived-religion.rerum.io/deer-lr/vocab/context.json",
-//             "http://www.w3.org/ns/anno.jsonld",
-//             "http://geojson.org/geojson-ld/geojson-context.jsonld"
-//         ]
-//         localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-//         const actualOutput = attributeChangedCallback(null, null, 'Annotation');
-//         expect(actualOutput).toEqual(expectedOutput);
-//     })
+    test("non-Array context", () => {
+        const userObj = {"@context": "http://lived-religion.rerum.io/deer-lr/vocab/context.json"}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'Annotation');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://lived-religion.rerum.io/deer-lr/vocab/context.json",
+            "http://www.w3.org/ns/anno.jsonld",
+            "http://geojson.org/geojson-ld/geojson-context.jsonld"
+        ]        
+        expect(actualOutput).toEqual(expectedOutput);
+    })
 
-//     test("no context", () => {
-//         const userObj = {
-//             "access": "checker without authentication",
-//             "@id": "https://store.rerum.io/v1/id/11111",
-//             "theres": "no restriction on these edits!"
-//         }
-//         const expectedOutput = [
-//             "http://www.w3.org/ns/anno.jsonld",
-//             "http://geojson.org/geojson-ld/geojson-context.jsonld"
-//         ]
-//         localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-//         const actualOutput = attributeChangedCallback(null, null, 'Annotation');
-//         expect(actualOutput).toEqual(expectedOutput);
-//     })
+    test("no context", () => {
+        const userObj = {
+            "access": "checker without authentication",
+            "@id": "https://store.rerum.io/v1/id/11111",
+            "theres": "no restriction on these edits!"
+        }
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'Annotation');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://www.w3.org/ns/anno.jsonld",
+            "http://geojson.org/geojson-ld/geojson-context.jsonld"
+        ]        
+        expect(actualOutput).toEqual(expectedOutput);
+    })
 
-//     test("random context", ()=> {
-//         const userObj = {"@context": "http://someAddress.json"}
-//         const expectedOutput = [
-//             "http://someAddress.json",
-//             "http://www.w3.org/ns/anno.jsonld",
-//             "http://geojson.org/geojson-ld/geojson-context.jsonld"
-//         ]
-//         localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-//         const actualOutput = attributeChangedCallback(null, null, 'Annotation');
-//         expect(actualOutput).toEqual(expectedOutput);
-//     })
+    test("random context", ()=> {
+        const userObj = {"@context": "http://someAddress.json"}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'Annotation');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://someAddress.json",
+            "http://www.w3.org/ns/anno.jsonld",
+            "http://geojson.org/geojson-ld/geojson-context.jsonld"
+        ]        
+        expect(actualOutput).toEqual(expectedOutput);
+    })
 
-//     test("context with URIs out of order", () => {
-//         const userObj = {"@context": [
-//             "http://geojson.org/geojson-ld/geojson-context.jsonld",
-//             "http://www.w3.org/ns/anno.jsonld"
-//         ]}
-//         const expectedOutput = [
-//             "http://www.w3.org/ns/anno.jsonld",
-//             "http://geojson.org/geojson-ld/geojson-context.jsonld"
-//         ]
-//         localStorageMock.getItem.mockReturnValue(JSON.stringify(userObj));
-//         const actualOutput = attributeChangedCallback(null, null, 'Annotation');
-//         expect(actualOutput).toEqual(expectedOutput);
-//     })
-// })
+    test("context with URIs out of order", () => {
+        const userObj = {"@context": [
+            "http://geojson.org/geojson-ld/geojson-context.jsonld",
+            "http://www.w3.org/ns/anno.jsonld"
+        ]}
+        localStorage.setItem('userResource', JSON.stringify(userObj));
+        instance.attributeChangedCallback(null, null, 'Annotation');
+        const actualOutput = JSON.parse(localStorage.getItem('newResource'))['@context'];
+        const expectedOutput = [
+            "http://www.w3.org/ns/anno.jsonld",
+            "http://geojson.org/geojson-ld/geojson-context.jsonld"
+        ]
+        expect(actualOutput).toEqual(expectedOutput);
+    })
+})

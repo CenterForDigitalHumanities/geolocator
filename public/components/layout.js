5f1882cf634662eb0462a8dab5e55f1eee7551ca
Tony Obradovic <tobradovic@slu.edu>
2023-12-12 14:25:26 -0600

    Update GeoJSON functionality... Switched to (long, lat) format. Added adjustments based on geometry type -- specifically polygon. Renamed PolyLine to LineString to fit GeoJSON format standards.

--- public/components/layout.js
+++ public/components/layout.js
@@ -256,7 +256,7 @@ class PointPicker extends HTMLElement {
         this.chooseGeometry(localStorage.getItem("geometryType"), true); 
 
         pointBtn.addEventListener("click", () => {this.chooseGeometry('Point', false); markerGroup.clearLayers()})
-        polylineBtn.addEventListener("click", () => {this.chooseGeometry('Polyline', false); markerGroup.clearLayers()})
+        polylineBtn.addEventListener("click", () => {this.chooseGeometry('LineString', false); markerGroup.clearLayers()})
         polygonBtn.addEventListener("click", () => {this.chooseGeometry('Polygon', false); markerGroup.clearLayers()})   
         
         previewMap.on('click', (e) => {
@@ -265,8 +265,8 @@ class PointPicker extends HTMLElement {
 
             let previouslySelectedCoords = localStorage.getItem('coordinates')
             previouslySelectedCoords = previouslySelectedCoords ? JSON.parse(previouslySelectedCoords) : [];
-            previouslySelectedCoords.push(e.latlng.lat)
             previouslySelectedCoords.push(e.latlng.lng)
+            previouslySelectedCoords.push(e.latlng.lat)
             localStorage.setItem('coordinates', JSON.stringify(previouslySelectedCoords))
 
             document.getElementById("confirmCoords").disabled = false
@@ -305,7 +305,7 @@ class PointPicker extends HTMLElement {
             pointBtn.style.border = "3px solid black";
             polygonBtn.style.border = "0px solid black";
             polylineBtn.style.border = "0px solid black";
-        } else if (newGeomChoice === "Polyline") {
+        } else if (newGeomChoice === "LineString") {
             pointBtn.style.border = "0px solid black";
             polygonBtn.style.border = "0px solid black";
             polylineBtn.style.border = "3px solid black";
@@ -328,13 +328,27 @@ class PointPicker extends HTMLElement {
         geo.type = geometry_type
         geo.coordinates = []
         for (let index = 0; index < coords.length; index += 2) {
-            let lat = parseInt(coords[index] * 1000000) / 1000000
-            let long = parseInt(coords[index+1] * 1000000) / 1000000
-            geo.coordinates.push([lat, long])
+            let long = parseInt(coords[index] * 1000000) / 1000000
+            let lat = parseInt(coords[index+1] * 1000000) / 1000000
+            geo.coordinates.push([long, lat])
         }
-        // Every geo type is an Array of arrays. Point is just an array.
-        if (geometry_type == 'Point') {
-            geo.coordinates = geo.coordinates[0]
+        switch(geometry_type){
+            case 'Point':
+                geo.coordinates = geo.coordinates[0]
+                break
+            case 'LineString':
+                geo.coordinates = geo.coordinates
+                break
+            case 'Polygon':
+                const first_point = geo.coordinates[0]
+                geo.coordinates.push(first_point)
+                let poly_array = []
+                poly_array.push(geo.coordinates)
+                geo.coordinates = poly_array
+                break
+            default:
+                geo.coordinates = geo.coordinates[0]
+                break
         }
         let geoJSON = {
             "type": "Feature",
